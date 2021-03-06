b4408a7fd46ac6b672e57a8df1d1be498042a5b5
aslac2020 <andredesenvolvedorti@gmail.com>
2021-03-06 18:31:56 -0300

    alteraçoes

---
+++ Calculadora/CalculadorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace CalculatorProgram
+{
+    public class CalculatorLibrary
+    {
+        public class Calculator
+        {
+            public Calculator()
+            {
+                StreamWriter logFile = File.CreateText("calculatora.log");
+                Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+                Trace.AutoFlush = true;
+                Trace.WriteLine("Starting Calculator Log");
+                Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+            }
+
+            public  double DoOperation(double num1, double num2, string op)
+            {
+                double result = double.NaN;
+
+                switch (op)
+                {
+                    case "sm":
+                        result = num1 + num2;
+                        Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                        break;
+                    case "st":
+                        result = num1 - num2;
+                        Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
+                        break;
+                    case "m":
+                        result = num1 * num2;
+                        Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
+                        break;
+                    case "d":
+                        // Ask the user to enter a non-zero divisor.
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                            Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                        }
+                        break;
+                    // Return text for an incorrect option entry.
+                    default:
+                        break;
+                }
+                return result;
+            }
+        }
+    }
+}
--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -1,52 +1,116 @@
 ﻿using System;
 
-namespace Calculadora
+namespace Calculator    
 {
-    class Program
+    class Calculator
     {
-        static void Main(string[] args)
+        public static double DoOperation(double num1, double num2, string op)
         {
-            int num1 = 0; int num2 = 0;
+            double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
 
-            Console.WriteLine("Minha Calculadora\n");
-            Console.WriteLine("---------------------\n");
-
-
-            //usuario digitar o nome
-            Console.WriteLine("Insira um número, e pressione Enter");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Insira outro número, e pressione Enter");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-
-            //Opçoes de selecão
-            Console.WriteLine("Selecione uma Opção:");
-            Console.WriteLine("\tsm - Soma");
-            Console.WriteLine("\tst - Subtração");
-            Console.WriteLine("\tm - Multiplicação");
-            Console.WriteLine("\td - Divisão");
-            Console.WriteLine("Sua opção?");
-
-            switch (Console.ReadLine())
+            // Use a switch statement to do the math.
+            switch (op)
             {
                 case "sm":
-                    Console.WriteLine($"Seu Resultado é: {num1} + {num2} = " + (num1 + num2));
+                    result = num1 + num2;
                     break;
                 case "st":
-                    Console.WriteLine($"Seu Resultado é: {num1} - {num2} = " + (num1 - num2));
+                    result = num1 - num2;
                     break;
                 case "m":
-                    Console.WriteLine($"Seu Resultado é: {num1} * {num2} = " + (num1 * num2));
+                    result = num1 * num2;
                     break;
                 case "d":
-                    Console.WriteLine($"Seu Resultado é: {num1} / {num2} = " + (num1 / num2));
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
                     break;
+                // Return text for an incorrect option entry.
                 default:
                     break;
             }
-
-
+            return result;
         }
     }
-}
+
+    public class Program
+       {
+            static void Main(string[] args)
+            {
+                bool endApp = false;
+
+                Console.WriteLine("Minha Calculadora\n");
+                Console.WriteLine("---------------------\n");
+
+            Calculator calculator = new Calculator();
+
+            while (!endApp)
+            {
+                //declarando variaveis
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+                Console.WriteLine("Insira um número, e pressione Enter:");
+                numInput1 = Console.ReadLine();
+
+
+                //primeiro numero
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("Valor inserido inválido, Favor entrar com um valor: ");
+                    numInput1 = Console.ReadLine();
+
+                }
+
+
+                Console.WriteLine("Insira outro número, e pressione Enter:");
+                numInput2 = Console.ReadLine();
+
+                //segundo numero
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("Valor inserido inválido, Favor entrar com um valor: ");
+                    numInput2 = Console.ReadLine();
+
+                }
+
+                //Opçoes de selecão
+                Console.WriteLine("Selecione uma Opção:");
+                Console.WriteLine("\tsm - Soma");
+                Console.WriteLine("\tst - Subtração");
+                Console.WriteLine("\tm - Multiplicação");
+                Console.WriteLine("\td - Divisão");
+                Console.WriteLine("Sua opção?");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("Esta operação resultará em um erro matemático. \n");
+                    }
+                    else Console.WriteLine("Seu resultado: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ah não! Ocorreu uma exceção ao tentar fazer as contas.\n - Detalhes:" + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                Console.Write("Pressione Enter para sair do aplicativo");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            return;
+            }
+        }
+    }
